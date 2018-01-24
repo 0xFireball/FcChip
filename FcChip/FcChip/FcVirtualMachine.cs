c842afe4f76af0f1bc0413cb7d007d592fe78785
0xFireball <0xFireball@outlook.com>
2018-01-24 12:21:04 -0800

    some execution support

---
+++ FcChip/FcChip/FcVirtualMachine.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+
+namespace FcChip {
+    public class FcVirtualMachine {
+        private Stream programStream;
+
+        public class Registers {
+            public ushort A;
+            public ushort B;
+
+            public ushort C;
+
+            public bool E;
+
+            public void Set(FcRegister registerId, ushort value) {
+                switch (registerId) {
+                    case FcRegister.A:
+                        A = value;
+                        break;
+                    case FcRegister.B:
+                        B = value;
+                        break;
+                    case FcRegister.C:
+                        C = value;
+                        break;
+                    case FcRegister.E:
+                        E = value > 0;
+                        break;
+                }
+            }
+
+            public ushort Get(FcRegister registerId) {
+                switch (registerId) {
+                    case FcRegister.A:
+                        return A;
+                    case FcRegister.B:
+                        return B;
+                    case FcRegister.C:
+                        return C;
+                    case FcRegister.E:
+                        return (ushort) (E ? 1 : 0);
+                    default:
+                        return 0;
+                }
+            }
+        }
+
+        public FcVirtualMachine() {
+            Initialize();
+        }
+
+        private Registers registers;
+
+        public void Initialize() {
+            registers = new Registers();
+        }
+
+        public void LoadProgram(Stream programStream) {
+            this.programStream = programStream;
+        }
+
+        public void Execute() {
+            while (true) {
+                programStream.Position = registers.C;
+                var opCodeByte = (byte) programStream.ReadByte();
+                if (!Enum.IsDefined(typeof(FcInternalOpCode), opCodeByte)) {
+                    // unrecognized opcode
+                    throw new FcVirtualMachineExecutionException(
+                        $"unrecognized opcode at offset {programStream.Position - 1}");
+                }
+
+                var machineOpCode = (FcInternalOpCode) opCodeByte;
+
+                ProcessInstruction(machineOpCode);
+            }
+        }
+
+        private void ProcessInstruction(FcInternalOpCode opCode) {
+            switch (opCode) {
+                case FcInternalOpCode.Nop:
+                    break;
+                case FcInternalOpCode.MovR: {
+                    var srcReg = (FcRegister) (byte) programStream.ReadByte();
+                    var destReg = (FcRegister) (byte) programStream.ReadByte();
+
+                    registers.Set(destReg, registers.Get(srcReg));
+
+                    break;
+                }
+                case FcInternalOpCode.MovV: {
+                    var srcVal = (byte) programStream.ReadByte();
+                    var destReg = (FcRegister) (byte) programStream.ReadByte();
+
+                    registers.Set(destReg, srcVal);
+
+                    break;
+                }
+                case FcInternalOpCode.AddR: {
+                    var srcReg = (FcRegister) (byte) programStream.ReadByte();
+                    registers.Set(FcRegister.A, (ushort) (registers.Get(FcRegister.A) + registers.Get(srcReg)));
+                    
+                    break;
+                }
+            }
+        }
+
+        public class FcVirtualMachineExecutionException : Exception {
+            public FcVirtualMachineExecutionException(string message) : base(message) { }
+        }
+    }
+}
--- FcChip/FcChipTool/Program.cs
+++ FcChip/FcChipTool/Program.cs
@@ -1,9 +1,32 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CommandLine;
+using FcChip;
 
 namespace FcChipTool {
     class Program {
+        [Verb("run", HelpText = "Run a program in bytecodeformat.")]
+        class RunOptions {
+            [Value(0, Min = 2)]
+            public IEnumerable<string> inputFiles { get; set; }
+        }
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<RunOptions>(args)
+                .MapResult(
+                    opts => ProgramRun(opts),
+                    errs => 1);
+        }
+
+        private static int ProgramRun(RunOptions opts) {
+            var inputStream = File.OpenRead(opts.inputFiles.Skip(1).First());
+
+            var vm = new FcVirtualMachine();
+            vm.LoadProgram(inputStream);
+            vm.Execute();
+            
+            return 0;
         }
     }
 }
