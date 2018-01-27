f56fa090acd1aaac4c88d0236509d346c8a4135a
0xFireball <0xFireball@outlook.com>
2018-01-26 23:13:27 -0800

    add chip dump feature to FcChipTool

--- FcChip/FcChipTool/Program.cs
+++ FcChip/FcChipTool/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using CommandLine;
@@ -10,9 +11,12 @@ namespace FcChipTool {
         class RunOptions {
             [Value(0, Min = 2)]
             public IEnumerable<string> inputFiles { get; set; }
-            
+
             [Option('m', "memory", Required = false, Default = -1, HelpText = "The memory size for the chip")]
             public long memorySize { get; set; }
+
+            [Option('d', "dump", Required = false, Default = false, HelpText = "Dump chip information after halt")]
+            public bool dump { get; set; }
         }
 
         static void Main(string[] args) {
@@ -29,10 +33,24 @@ namespace FcChipTool {
             if (opts.memorySize > 0) {
                 chip.memorySize = (uint) opts.memorySize;
             }
+
             chip.initialize();
             chip.loadProgram(inputStream);
             while (chip.state != FcVirtualChip.State.Stopped) {
                 chip.tick();
+            }
+
+            if (opts.dump) {
+                Console.WriteLine("== MEMORY DUMP ==");
+                for (var i = 0; i < chip.memory.Length; i++) {
+                    Console.Write($"{chip.memory[i]:x2} ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("== REGISTER DUMP ==");
+                var registerIds = (FcRegister[]) Enum.GetValues(typeof(FcRegister));
+                foreach (var registerId in registerIds) {
+                    Console.WriteLine($"{registerId} {chip.registers.Get(registerId):x4}");
+                }
             }
 
             return 0;
